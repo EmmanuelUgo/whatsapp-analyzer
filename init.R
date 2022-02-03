f6cc09f5460cc68db0969743f0a030f592c8de75
Emmanuel Ugochukwu <47338860+EmmanuelUgo@users.noreply.github.com>
2022-02-03 22:48:04 +0100

    Update init.R

--- init.R
+++ init.R
@@ -4,7 +4,7 @@
 # Example R code to install packages if not already installed
 #
 
-my_packages = c(shiny, shiny.semantic, semantic.dashboard, rwhatsapp, wordcloud, tidytext, plotly)
+my_packages = c("shiny", "shiny.semantic", "semantic.dashboard", "rwhatsapp", "wordcloud", "tidytext", "plotly")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
